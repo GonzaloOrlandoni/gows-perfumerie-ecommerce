d139247fbf3998e08211c1c4e7ca0c6775061402
Gonzalo Orlandoni <gonzaloorlandoni1@gmail.com>
2026-04-30 19:25:04 -0300

    fix: configurar ruta base dinámica para Vercel y GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gows-perfumerie-ecommerce/',
+  base: process.env.VERCEL ? '/' : '/gows-perfumerie-ecommerce/',
 })
